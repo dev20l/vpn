27e3da1606d625518d2a270d5ad0f0d633283379
dev20l <iphonetasli@gmail.com>
2024-09-25 09:33:27 -0700

    Update v.js

--- v.js
+++ v.js
@@ -1,1 +1,3 @@
 <!-- © 2023 جميع الحقوق محفوظة. هذه الصفحة محمية بحقوق الملكية. البريد الإلكتروني للمصمم: victortassli@gmail.com -->   
+<!-- © 2023 All rights reserved. This page is protected by copyright. Designer's email: victortassli@gmail.com -->
+ <!-- fb: https://www.facebook.com/1victorabdo -->
